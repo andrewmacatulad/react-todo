148b47554226b9284a54a25eb56966a6e9be78b9
Marc Andrew <andrewmacatulad@gmail.com>
2017-05-02 14:03:43 +0800

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -4,12 +4,13 @@ var firebase = require('firebase');
 // Initialize Firebase
 try {
   var config = {
-    apiKey: "AIzaSyBMjto7-saVq8ZPmqnaVBgvifsBL5309qU",
-    authDomain: "andrew-todo-app.firebaseapp.com",
-    databaseURL: "https://andrew-todo-app.firebaseio.com",
-    projectId: "andrew-todo-app",
-    storageBucket: "andrew-todo-app.appspot.com",
-    messagingSenderId: "309761678461"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+
   };
   firebase.initializeApp(config);
 } catch(e) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
+var envFile = require('node-env-file')
+
 
 // Environment variable
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'))
+} catch (e){
+
+}
 
 module.exports = {
   entry: [
@@ -22,6 +31,17 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
       }
     })
   ],
